1ed162a1b8c28299a219b62314ee7b40ce739a2f
Lloyd Nanka <lloydnanka@gmail.com>
2017-08-28 15:25:10 +1000

    sdfs

--- Startup.cs
+++ Startup.cs
@@ -27,6 +27,8 @@ namespace aspnetapp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {   
+            string projectId = "lloyd-test";
+
             services.AddGoogleTrace(options =>
             {
                 options.ProjectId = projectId;
