98b3386d57f51f91194964bb74839fa9ba19f252
Lloyd Nanka <lloydnanka@gmail.com>
2017-08-28 17:05:03 +1000

    ffaaAaa

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace aspapp2.Controllers
+{
+    public class HomeController : Controller
+    {
+
+      private readonly ILogger _logger;
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Index()
+        {
+                try
+                {
+                    throw new Exception("this is shiiite");
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(500, ex, ex.Message);
+                }
+                return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+            
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
